327cd1b4b864e0946456e63353cd6f79b9080733
glekovkirilll <glekovkirill2016@gmail.com>
2022-04-13 14:33:58 +0700

    added xlsx

--- ProcessAuto/Controllers/StudentsRegistrationController.cs
+++ ProcessAuto/Controllers/StudentsRegistrationController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ExcelDataReader;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ProcessAuto.Areas.Identity.Data;
 using ProcessAuto.Models;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ProcessAuto.Controllers
 {
     public class StudentsRegistrationController : Controller
     {
+        private readonly UserManager<PAUser> _userManager;
+        private readonly SignInManager<PAUser> _signInManager;
+
+        public StudentsRegistrationController(UserManager<PAUser> userManager, SignInManager<PAUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
         // GET: StudentsRegistrationController
         [HttpGet]
         public ActionResult Index()
@@ -14,9 +29,46 @@ namespace ProcessAuto.Controllers
             return View(new List<Student>());
         }
 
-        // GET: StudentsRegistrationController/Details/5
-        public ActionResult Details(int id)
+        [HttpPost]
+        public IActionResult Index(IFormCollection form)
         {
+            List<Student> students = new List<Student>();
+            var fileName = "./Users.xlsx";
+            // For .net core, the next line requires the NuGet package, 
+            // System.Text.Encoding.CodePages
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var stream = System.IO.File.Open(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+
+                    while (reader.Read()) //Each row of the file
+                    {
+                        students.Add(new Student
+                        {
+                            Surname = reader.GetValue(0).ToString(),
+                            Name = reader.GetValue(1).ToString(),
+                            MiddleName = reader.GetValue(2).ToString(),
+                            Email = reader.GetValue(3).ToString(),
+                            Password = reader.GetValue(4).ToString()
+                        });
+                    }
+                }
+            }
+            return View(students);
+        }
+
+        // GET: StudentsRegistrationController/Details/5
+        [HttpPost]
+        public async Task<IActionResult> Register(List<Student> students)
+        {
+            foreach (var Student in students) 
+            { 
+                PAUser user = new PAUser { Surname = Student.Surname, Name = Student.Name, MiddleName = Student.MiddleName, Email = Student.Email};
+                var result = await _userManager.CreateAsync(user, Student.Password);
+            }
+
+
             return View();
         }
 
